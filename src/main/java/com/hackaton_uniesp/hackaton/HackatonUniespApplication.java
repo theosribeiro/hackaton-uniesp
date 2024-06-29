2b54129220387442c59793c0c1ad6f4bdbb47b63
theosribeiro <theosribeiro@gmail.com>
2024-06-29 11:50:09 -0300

    inicializando

---
+++ src/main/java/com/hackaton_uniesp/hackaton/HackatonUniespApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton_uniesp.hackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonUniespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonUniespApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackaton_uniesp/hackaton/HackatonUniespApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton_uniesp.hackaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonUniespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
